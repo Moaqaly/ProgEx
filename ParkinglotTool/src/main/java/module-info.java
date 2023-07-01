10bdcd78636832241f5f34b8ff3a4859b7949575
Moaqaly <108799424+Moaqaly@users.noreply.github.com>
2023-07-01 21:54:03 +0200

    Add files via upload

---
+++ ParkinglotTool/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.parkinglottool {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires AnimateFX;
+    requires java.sql;
+    requires jBCrypt;
+
+    opens Home_page_user;
+    opens Login;
+    opens Home_page_admin;
+    opens Add_User;
+
+}
